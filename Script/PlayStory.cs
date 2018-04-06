646fa565b8aeda941fe181af55709b9e347dfa6d
jialingkun <djaka.tingkir2@gmail.com>
2018-04-06 14:30:59 +0700

    Choices Tampil

--- Script/PlayStory.cs
+++ Script/PlayStory.cs
@@ -19,10 +19,17 @@ public class PlayStory : MonoBehaviour {
 	//animate
 	private bool typingAnimation;
 	public float textSpeed = 0.03f;
+	public float transitionDelay = 1.1f;
 
 
-	//Gameobject;
+	//other Gameobject;
 	private Text dialogText;
+	private Image illustrationImage;
+	private Image fadePanel;
+	//choice
+	private string[] choiceDestination;
+	private Text[] choiceText;
+	private int currentChoicesCount;
 
 	// Use this for initialization
 	void Start () {
@@ -76,11 +83,24 @@ public class PlayStory : MonoBehaviour {
 
 
 		//temp initialize
-		currentCommandIndex = 0;
+		currentCommandIndex = -1; //-1 because currentCommandIndex++ run first
 		currentScene = firstScene;
+
 
 		//gameobject initialize
 		dialogText = GameObject.Find("DialogText").GetComponent<Text>();
+		illustrationImage = GameObject.Find("Illustration").GetComponent<Image>();
+		fadePanel = GameObject.Find("FadePanel").GetComponent<Image>();
+
+		//choice initialize
+		int maxChoices = 6;
+		choiceDestination = new string[maxChoices];
+		choiceText = new Text[maxChoices];
+		for (int i = 0; i < maxChoices; i++) {
+			choiceText[i] = GameObject.Find("Choice"+(i+1)+"Text").GetComponent<Text>();
+			choiceText [i].transform.parent.gameObject.SetActive (false);
+		}
+
 
 		//start dialog
 		clickDialog();
@@ -101,16 +121,46 @@ public class PlayStory : MonoBehaviour {
 		dialogText.text = tempLine[0];
 		typingAnimation = false;
 	}
+
+	IEnumerator IllustrationTransition(Sprite sprite){
+		fadePanel.CrossFadeAlpha(1.0f, 0.7f, false); //(alpha value, fade speed, not important)
+		yield return new WaitForSeconds(transitionDelay);
+		illustrationImage.sprite = sprite;
+		fadePanel.CrossFadeAlpha(0.0f, 0.7f, false);
+		yield return new WaitForSeconds(1.0f);
+		clickDialog ();
+	}
 	
 	public void clickDialog(){
 		if (typingAnimation) {
 			SkipAnimateText ();
 		} else {
+			currentCommandIndex++;
 			tempLine = scene [currentScene].commands [currentCommandIndex].Split(":"[0]);
 			if (tempLine.Length > 1) {
 				if (tempLine [0].Trim () == "choices") {
-					currentCommandIndex--;
+					currentChoicesCount = scene [currentScene].choices.Count;
+					if (currentChoicesCount > 2) {
+						for (int i = 0; i < currentChoicesCount; i++) {
+							choiceDestination [i] = scene [currentScene].choices [i].destination;
+							choiceText [i].text = scene [currentScene].choices [i].text;
+							choiceText [i].transform.parent.gameObject.SetActive (true);
+						}
+					} else if (currentChoicesCount == 2) {
+						//center top
+						choiceDestination [1] = scene [currentScene].choices [1].destination;
+						choiceText [1].text = scene [currentScene].choices [1].text;
+						choiceText [1].transform.parent.gameObject.SetActive (true);
+						//center bottom
+						choiceDestination [4] = scene [currentScene].choices [4].destination;
+						choiceText [4].text = scene [currentScene].choices [4].text;
+						choiceText [4].transform.parent.gameObject.SetActive (true);
+					}
+					dialogText.transform.parent.gameObject.SetActive (false);
+				} else if (tempLine [0].Trim () == "image") {
+					StartCoroutine(IllustrationTransition(illustration [tempLine [1].Trim ()]));
 				} else { //not command but contain :
+					tempLine[0] = scene [currentScene].commands [currentCommandIndex]; //unsplit
 					StartCoroutine(AnimateText(tempLine[0]));
 				}
 			} else if(tempLine [0].Trim () == "gameover") { //gameover
@@ -118,10 +168,11 @@ public class PlayStory : MonoBehaviour {
 			} else { //dialog
 				StartCoroutine(AnimateText(tempLine[0]));
 			}
-			currentCommandIndex++;
+
 		}
 
+	}
 
-
+	public void clickChoice(int destinationIndex){
 	}
 }
